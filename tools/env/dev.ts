05f70f409108b238497f9b81f8478f56cdbba0e9
hoangle <hoangle@Innoria.local>
2017-11-01 15:24:13 +0700

    change config

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -2,9 +2,9 @@ import { EnvConfig } from './env-config.interface';
 
 const DevConfig: EnvConfig = {
   ENV: 'DEV',
-  PARSE_SERVER_URL: 'http://localhost:3778/socialobe-api',
-  PARSE_SERVER_URL_HTTPS: 'https://localhost:3778/socialobe-api',
-  APP_ID: 'Socialobe-Test-APPID'
+  PARSE_SERVER_URL: 'http://localhost:3000/parse',
+  PARSE_SERVER_URL_HTTPS: 'https://localhost:3000/parse',
+  APP_ID: 'luan-van-app-id'
 };
 
 export = DevConfig;
