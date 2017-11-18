d4888f1fa4c860e75f46dbea10f8a5a247b2745e
hoangle <hoangle@Innoria.local>
2017-11-18 18:30:32 +0700

    update merge

--- src/client/app/shared/directives/directive.module.ts
+++ src/client/app/shared/directives/directive.module.ts
@@ -7,21 +7,11 @@ import { TagsInputDirective } from './tagsinput.directive';
 import { AutoCompleteDirective } from './autocomplete.directive';
 import { CaretDetectDirective } from './caret-detect.directive';
 import { DataClipboardTargetDirective } from './data-clipboard-target.directive';
-<<<<<<< HEAD
-import { MaterialSelectDirective } from './material-select.directive'
-import { MaterialTimePickerDirective } from './material-time-picker.directive'
+import { MaterialSelectDirective } from './material-select.directive';
+import { MaterialTimePickerDirective } from './material-time-picker.directive';
 @NgModule({
   imports: [FormsModule],
   declarations: [ValidateOnBlurDirective, ScrollDirective, TagsInputDirective, AutoCompleteDirective, CaretDetectDirective, DataClipboardTargetDirective, MaterialSelectDirective,MaterialTimePickerDirective],
   exports: [ValidateOnBlurDirective, ScrollDirective, TagsInputDirective, AutoCompleteDirective, CaretDetectDirective, DataClipboardTargetDirective, MaterialSelectDirective,MaterialTimePickerDirective]
-=======
-import { MaterialSelectDirective } from './material-select.directive';
-import { MaterialmodalDirective } from './material-modal.directive';
-
-@NgModule({
-  imports: [FormsModule],
-  declarations: [ValidateOnBlurDirective, ScrollDirective, TagsInputDirective, AutoCompleteDirective, CaretDetectDirective, DataClipboardTargetDirective, MaterialSelectDirective, MaterialmodalDirective],
-  exports: [ValidateOnBlurDirective, ScrollDirective, TagsInputDirective, AutoCompleteDirective, CaretDetectDirective, DataClipboardTargetDirective, MaterialSelectDirective, MaterialmodalDirective]
->>>>>>> 17153117312a92f35c972dfa6f40f28c95838c7b
 })
 export class DirectiveModule { }
